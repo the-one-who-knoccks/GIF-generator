ea93a3c0d472c7f3a39e2602a316b6d43f813e4f
Thiago Alves <the-one-who-knoccks@pop-os.localdomain>
2020-09-24 23:01:44 -0300

    :sparkles: Enviroment configs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Random from './Components/Random_V1';
-import Tag from './Components/Tag_V1';
+import Random from './Components/Random_V2';
+import Tag from './Components/Tag_V2';
 
 import './App.css';
 
